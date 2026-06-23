cfcd2deb2789a5df98f72074b412ece29319f40d
ThisGuyCaleb <152720432+ThisGuyCaleb@users.noreply.github.com>
2026-06-23 18:22:11 -0400

    Update hamster_wheel_scene.gd

--- scripts/wheel/hamster_wheel_scene.gd
+++ scripts/wheel/hamster_wheel_scene.gd
@@ -8,7 +8,7 @@ const HAMSTER_WHEEL_SLOT = preload("res://scenes/wheel/hamster_wheel_slot.tscn")
 
 func _ready() -> void:
 	GScript.add_wheel.connect(reveal_wheel)
-	
+	# generate wheels
 func setup_wheels() -> void:
 	for i in range(GScript.num_of_wheels):
 		var margin_container := MarginContainer.new()
@@ -22,7 +22,6 @@ func reveal_wheel() -> void:
 	var margin_container := MarginContainer.new()
 	margin_container.set_mouse_filter(Control.MOUSE_FILTER_IGNORE)
 	var slot = HAMSTER_WHEEL_SLOT.instantiate()
-	#margin_container.add_theme_constant_override("margin_right", 10)
 	margin_container.size_flags_horizontal = Control.SIZE_EXPAND_FILL
 	margin_container.add_child(slot)
 	hamster_wheel_slots.add_child(margin_container)
