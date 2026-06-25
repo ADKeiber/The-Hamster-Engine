55abb5440b568552544f749f634b07fed4a0be0c
ThisGuyCaleb <152720432+ThisGuyCaleb@users.noreply.github.com>
2026-06-25 07:26:02 -0400

    Project Modules Sprite added
    Project Modules 1 and 2 sprite animations added

--- scripts/hamster/stats_menu.gd
+++ scripts/hamster/stats_menu.gd
@@ -13,4 +13,3 @@ func stat_setup() -> void:
 	stamina_stat.text = "Stamina: " + str(hamster.stats.stamina)
 	speed_stat.text = "Speed: " + str(hamster.stats.speed)
 	trait_stat.text = "Trait: " + hamster.hamster_trait["Name"] + " " + hamster.hamster_trait["Description"]
-	
