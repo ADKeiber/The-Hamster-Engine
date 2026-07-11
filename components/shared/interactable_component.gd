15a1dfd485c7a341e70c5174e9fe5eb18fd6bcff
ThisGuyCaleb <152720432+ThisGuyCaleb@users.noreply.github.com>
2026-07-11 17:30:04 -0400

    interactable
    interactable stuff

--- components/shared/interactable_component.gd
+++ components/shared/interactable_component.gd
@@ -8,7 +8,6 @@ signal off
 @export var area : Area2D
 var hamster : Hamster
 @export var locked_in : bool
-var occupied = false
 var hovering_hamster
 
 func _ready() -> void:
@@ -16,10 +15,10 @@ func _ready() -> void:
 	area.area_exited.connect(self.area_exited)
 
 func area_entered(hamster_area : Area2D) -> void:
-	if occupied == false && hamster_area.get_parent() is Hamster:
+	if check_child_type(self, Hamster) == false && hamster_area.get_parent() is Hamster:
+		if hamster_area.get_parent().draggable_component.drag_ended.is_connected(self.reparent_hamster):
+			hamster_area.get_parent().draggable_component.drag_ended.disconnect(self.reparent_hamster)
 		hovering_hamster = hamster_area
-		#if hovering_hamster.get_parent().draggable_component.drag_ended.is_connected(self.reparent_hamster):
-			#hovering_hamster.get_parent().draggable_component.drag_ended.disconnect(self.reparent_hamster)
 		hovering_hamster.get_parent().draggable_component.drag_ended.connect(self.reparent_hamster)
 
 
@@ -36,11 +35,10 @@ func reparent_hamster() -> void:
 	hamster.animation_component.invisible()
 	hamster.global_position = self.global_position
 	on.emit()
-	occupied = true
+	if hamster.draggable_component.drag_started.is_connected(self.power_off):
+			hamster.draggable_component.drag_started.disconnect(self.power_off)
 	#get_parent().building_resource.interact(hamster) For future building trigger use 
 	if locked_in == false:
-		if hamster.draggable_component.drag_started.is_connected(self.power_off):
-			hamster.draggable_component.drag_started.disconnect(self.power_off)
 		hamster.draggable_component.drag_started.connect(self.power_off)
 	elif locked_in == true: # lockes hamster into machine
 		hamster.draggable_component.draggable = false
@@ -49,6 +47,13 @@ func reparent_hamster() -> void:
 func power_off() -> void: #when hamster is removed
 	off.emit()
 	hamster.animation_component.visible()
-	occupied = false
 	if hamster.draggable_component.drag_started.is_connected(self.power_off):
 			hamster.draggable_component.drag_started.disconnect(self.power_off)
+	print("power off")
+
+
+func check_child_type(node, type) -> bool:
+	for child in node.get_children():
+		if is_instance_of(child, type):
+			return true
+	return false
