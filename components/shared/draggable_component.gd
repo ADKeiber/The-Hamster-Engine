7ff7d1aebeaebd76acd68eede56aa908249b7008
ThisGuyCaleb <152720432+ThisGuyCaleb@users.noreply.github.com>
2026-07-12 08:28:29 -0400

    last interactable component stuff
    c

--- components/shared/draggable_component.gd
+++ components/shared/draggable_component.gd
@@ -38,9 +38,21 @@ func _on_area_input_event(_viewport, event, _shape_idx):
 		else:
 			dragging = false
 			drag_ended.emit(self)
+			hamster_check()
 
 #returns to pickup position.. Procs if hamster either "fails to be placed" or "fails to interact" something along those lines
 func failed_to_drop() -> void:
 	if snap_back_on_fail:
 		get_parent().global_position = start_drag_location
 	draggable = true
+
+func hamster_check() -> void:
+	var hamster = get_parent()
+	if hamster is Hamster:
+		var ovelapped = area.get_overlapping_areas()
+		if area.get_overlapping_areas().is_empty():
+			failed_to_drop()
+		for areas in ovelapped:
+			if not area.is_in_group("Building"):
+				failed_to_drop()
+	else: return
